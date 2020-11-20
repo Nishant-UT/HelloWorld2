d517ebc3f3666cfa8b2c7c6de58921c5bde792eb
Nishant <nishantvk47@gmail.com>
2020-11-20 20:50:53 +0530

    Second Commit

---
+++ Paint Estimator/src/paintEstimator/PaintEstimator.java
@@ -0,0 +1,26 @@
+package paintEstimator;
+import java.util.Scanner;
+import java.lang.Math;
+public class PaintEstimator {
+	public static void main(String[] args) {
+	      Scanner scnr = new Scanner(System.in);
+	      double wallHeight;
+	      double wallWidth;
+	      double wallArea;
+	      
+	      System.out.println("Enter wall height (feet):");
+	      wallHeight = scnr.nextDouble();
+	      
+	      System.out.println("Enter wall width (feet):");
+	      wallWidth=scnr.nextDouble();
+	      
+	      wallArea = wallWidth * wallHeight;
+	      System.out.println("Wall area: "+wallArea+" square feet");
+
+	      double gallons=wallArea/350.0;
+	      System.out.println("Paint needed: "+gallons+" gallons");
+	      System.out.println("Cans needed: "+Math.round(gallons)+" can(s)");
+	   }
+}
+	
+
